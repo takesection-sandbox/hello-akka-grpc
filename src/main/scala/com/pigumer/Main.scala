17ba8c8f2feb79f9d89f17fd712e86a285e5a43a
Shigeki Shoji <takesection@gmail.com>
2023-01-12 16:42:28 +0900

    3rd

--- src/main/scala/com/pigumer/Main.scala
+++ src/main/scala/com/pigumer/Main.scala
@@ -1,24 +1,31 @@
 package com.pigumer
 
-import akka.actor.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorSystem, Behavior}
 import akka.http.scaladsl.Http
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
 object Server {
-  def apply(implicit system: ActorSystem) {
+  def setup: Behavior[Any] = Behaviors.setup { ctx =>
+    implicit val system = ctx.system
+
     val helloService = HelloServiceHandler.withServerReflection(new HelloServiceImpl())
 
     Http().
       newServerAt("127.0.0.1", -1).
       bind(helloService)
+
+    Behaviors.same
   }
 }
 
 object Main extends App {
-  val conf = ConfigFactory
-    .parseString("akka.http.server.preview.enable-http2 = on")
+  val conf = ConfigFactory.load()
     .withFallback(ConfigFactory.defaultApplication())
 
-  val system = ActorSystem("hello", conf)
-  Server(system)
+  val system = ActorSystem(Server.setup, "hello", conf)
+  Await.ready(system.whenTerminated, Duration.Inf)
 }
